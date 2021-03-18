6b01bf9f09ef8ab5fb803fbe8b31539f5c8bcc5a
Joshhortt <joshhortt@yahoo.com>
2021-03-18 19:25:23 +0000

    Add new Project 01 Homework 'MathConsoleHome' about 'ClassLibraryProjectTypes'.

---
+++ 01.ClassLibraryProjectType/MathConsoleHome/MathConsoleHome/Program.cs
@@ -0,0 +1,39 @@
+﻿// 01 - HOMEWORK - Class Library Project Type
+// Build a .NET Standard class Library and a Console Application.
+// Put one or some calculation methods in it and call it from the console.
+using System;
+using StandardLibrary;
+
+namespace MathConsoleHome
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Hello User!");
+
+            // Read the first number   
+            Console.Write("Enter a Number: ");
+            var read1 = Console.ReadLine();
+            // Convert a string to an int  
+            int numb1 = Convert.ToInt32(read1);
+            // Read the second number  
+            Console.Write("Enter another Number: ");
+            var read2 = Console.ReadLine();
+            // Convert string to int  
+            int numb2 = Convert.ToInt32(read2);
+            // Read operation type - operator  
+            Console.Write("Enter Operator: '+' for add, '-' for sub, '*' for mult,  '/' for div) ");
+            var op = Console.ReadLine();
+
+            // Create a class library object and call the method CalculateTwoNumbers
+            MathLesson math = new MathLesson();
+            double result = math.CalculateTwoNumbers(numb1, numb2, op);
+
+            Console.WriteLine("Result: {0}", result);
+
+            Console.WriteLine("Press any key to exit Console....");
+            Console.ReadKey(true);
+        }
+	}
+}
---
+++ 01.ClassLibraryProjectType/MathConsoleHome/StandardLibrary/MathLesson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StandardLibrary;
+
+namespace StandardLibrary
+{
+	public class MathLesson
+	{
+        public MathLesson() { }
+
+        public double CalculateTwoNumbers(double numb1, double numb2, string op)
+        {
+            double result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = numb1 + numb2;
+                    break;
+                case "-":
+                    result = numb1 - numb2;
+                    break;
+                case "*":
+                    result = numb1 * numb2;
+                    break;
+                case "/":
+                    result = (double)numb1 / numb2;
+                    break;
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
+}
