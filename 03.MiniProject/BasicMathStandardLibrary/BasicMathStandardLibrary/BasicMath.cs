3953a715ace93889b322472da1a00fb82e777f90
Joshhortt <joshhortt@yahoo.com>
2021-03-19 19:59:53 +0000

    Add Mini-Project 03 Homework 'BasicMath' about unit tests, applying test-driven-development.

---
+++ 03.MiniProject/BasicMathStandardLibrary/BasicMathStandardLibrary/BasicMath.cs
@@ -0,0 +1,37 @@
+﻿// HOMEWORK - 03 - Mini-Project - Basic Bath (Unit Tests)
+// Build a Class Library (.NET Core Standard) with a class that does the basic math operations.
+// Create unit tests for all methods.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMathStandardLibrary
+{
+    public class BasicMath
+    {
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        public double Substract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Divide(double num1, double num2)
+        {
+            // return num1 / num2;
+            double result = 0;
+
+            if (num2 != 0)
+            {
+                result = num1 / num2;
+            }
+            return result;
+        }
+        public double Multiply(double num1, double num2)
+        {
+            // To trace error while testing, writing + operator instead of * operator.  
+            return num1 * num2;
+        }
+    }
+}
---
+++ 03.MiniProject/BasicMathStandardLibrary/BasicMathTest/MathUnitTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Xunit;
+using BasicMathStandardLibrary;
+
+namespace BasicMathTest
+{
+	public class MathUnitTest
+	{
+		[Theory]
+        [InlineData(4, 2, 6)]
+        [InlineData(6, 2, 8)]
+        [InlineData(2, -2, 0)]
+        [InlineData(100, -101, -1)]
+        public void Test_AddMethod(double num1, double num2, double expected)
+        {
+            BasicMath bm = new BasicMath();
+            double actual = bm.Add(num1, num2);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData(4, 2, 2)]
+        [InlineData(40, 2, 38)]
+        [InlineData(1, 2, -1)]
+        [InlineData(-4, 2, -6)]
+
+        public void Test_SubstractMethod(double num1, double num2, double expected)
+        {
+            BasicMath bm = new BasicMath();
+            double actual = bm.Substract(num1, num2);
+            Assert.Equal(expected, actual); ;
+        }
+
+        [Theory]
+        [InlineData(40, 2, 20)]
+        [InlineData(0, 0, 0)]
+        [InlineData(4, 2, 2)]
+        [InlineData(4, 0, 0)]  // need implementation because of infinitive number returned by this operation
+        public void Test_DivideMethod(double num1, double num2, double expected)
+        {
+            BasicMath bm = new BasicMath();
+            double actual = bm.Divide(num1, num2);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4, 2, 8)]
+        [InlineData(1, 1, 1)]
+        [InlineData(40, 1, 40)]
+        [InlineData(3, 3, 9)]
+        public void Test_MultiplyMethod(double num1, double num2, double expected)
+        {
+            BasicMath bm = new BasicMath();
+            double actual = bm.Multiply(num1, num2);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
