77165646deaf380894b71167662402b3bd3beb43
Joshhortt <joshhortt@yahoo.com>
2021-03-19 16:15:18 +0000

    Add comments to 'simple unit test'. example.

--- 02.UnitTest/ValidatorsPasswordLibrary/PasswordValidatorTests/ValidityTests.cs
+++ 02.UnitTest/ValidatorsPasswordLibrary/PasswordValidatorTests/ValidityTests.cs
@@ -1,18 +1,13 @@
-//The code below of the '' Test Driven Testing'  shows one single method, ValidatePassword(), in place of the
-//two methods implemented before. 
-
-//This method has two parameters: password and expectedResult. And the application of the Arrange-Act-Assert pattern 
-//is based on these parameters.
-
-//In addition, you see a set of attributes decorating the method. The first attribute marks the 
-//ValidatePassword() method as a theory.The other InlineData attributes represent the data to pass to the method. 
-
-//Each attribute has a couple of values that are mapped to the method's parameters. 
-
-//So, basically, the first value of each InlineData attribute is a possible password, 
-//and the second value is the boolean value expected as a result of the IsValid() method.
-
-//In other words, each InlineData attribute represents one invocation of the ValidatePassword() test.
+/*he code below of the example using' Test Driven Testing' approach shows one single method, ValidatePassword(), 
+in place of thetwo methods implemented before. 
+This method has two parameters: password and expectedResult. And the application of the Arrange-Act-Assert pattern 
+is based on these parameters.
+In addition, you see a set of attributes decorating the method. The first attribute marks the 
+ValidatePassword() method as a theory.The other InlineData attributes represent the data to pass to the method. 
+Each attribute has a couple of values that are mapped to the method's parameters. 
+So, basically, the first value of each InlineData attribute is a possible password,
+and the second value is the boolean value expected as a result of the IsValid() method.
+In other words, each InlineData attribute represents one invocation of the ValidatePassword() test.*/
 
 using System;
 using Xunit;
@@ -46,6 +41,18 @@ namespace PasswordValidatorTests
     }
 }
 /*
+In this exmaple of Unit testing, the two cases of password validity tested by the unit tests are 
+far from exhaustive.
+They are just two simple examples of positive and negative cases, but, of course, 
+the possible cases to test are many more. 
+You cannot expect to check every possible case, but you can test a significant subset of typical cases. 
+This helps in having a greater code coverage for your production code. 
+In the password validation example, this means that you hould identify a representative set of valid and invalid passwords.
+For each password in these sets, you should apply one of the tests implemented above.
+This approach should ensure significant confidence in the correct behavior of the IsValid() method. 
+But it requires to replicate the same code for each sample password to test. You know that code replication 
+is not a good practice. 
+
 using System;
 using Xunit;
 using ValidatorsPasswordLibrary;
