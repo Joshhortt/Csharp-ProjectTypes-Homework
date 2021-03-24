f54d081648a1f376e2f65b3ec7e208220187e07a
Joshhortt <joshhortt@yahoo.com>
2021-03-24 15:58:59 +0000

    Add complete styling and event handler implementation to homework of Mini Project 07.

--- 07.MiniProject/WpfOrderFormApp/WpfOrderForm/MainWindow.xaml.cs
+++ 07.MiniProject/WpfOrderFormApp/WpfOrderForm/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿// HOMEWORK - 07 - Mini-Project - WPF ORDER FORM
+using System;
 using System.Collections.Generic;
 using System.Linq;
-// HOMEWORK - 07 - Mini-Project - WPF ORDER FORM
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,5 +25,42 @@ namespace WpfOrderForm
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"The description is: {this.DescriptionText.Text}");
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WeldCheckbox.IsChecked = this.AssemblyCheckbox.IsChecked = this.PlasmaCheckbox.IsChecked = this.LaserCheckbox.IsChecked = this.PurchaseCheckbox.IsChecked =
+                this.LatheCheckbox.IsChecked = this.DrillCheckbox.IsChecked = this.FoldCheckbox.IsChecked = this.RollCheckbox.IsChecked = this.SawCheckbox.IsChecked = false;
+        }
+
+        private void Checkbox_Checked(object sender, RoutedEventArgs e)
+        {
+            this.LengthText.Text += ((CheckBox)sender).Content;
+        }
+
+        private void FinishDropdown_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (this.NoteText == null)
+                return;
+
+            var combo = (ComboBox)sender;
+            var value = (ComboBoxItem)combo.SelectedValue;
+
+            this.NoteText.Text = (string)value.Content;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            FinishDropdown_SelectionChanged(this.FinishDropdown, null);
+        }
+
+        private void SupplierNameText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            this.MassText.Text = this.SupplierNameText.Text;
+        }
+    }
 }
