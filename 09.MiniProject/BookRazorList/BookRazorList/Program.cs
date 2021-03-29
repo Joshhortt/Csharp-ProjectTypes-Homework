6a15ec71dc206529558de30879dc561b166ce693
Joshhortt <joshhortt@yahoo.com>
2021-03-29 18:47:57 +0100

    First Commit Mini-Project 09 Homework 'BookRazorList' build with ASP .NET Core 5. Razor Pages.

---
+++ 09.MiniProject/BookRazorList/BookRazorList/Program.cs
@@ -0,0 +1,31 @@
+// HOMEWORK - 09 - Mini-Project - ASP .NET Core Razor Pages
+// Build a ASP .NET Core Razor Pages App Book List. 
+// Implement CRUD operations.
+
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookRazorList
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
