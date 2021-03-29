3b42f65113bc16ba4dc39c408efb31a97f2b9dc1
Joshhortt <joshhortt@yahoo.com>
2021-03-29 20:06:20 +0100

    include Entityframework Database packages into Configuration Pipeline .

---
+++ 09.MiniProject/BookRazorList/BookRazorList/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookRazorList.Model
+{
+	public class ApplicationDbContext : DbContext  // 8. 
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) // 9. Constructor
+		{
+
+		}
+
+		public DbSet<TheBook> TheBook { get; set; }  // 10.
+	}
+}
