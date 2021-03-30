c6a4b0e4240fe65e511bb6735f569c773d9e1dc7
Joshhortt <joshhortt@yahoo.com>
2021-03-30 12:48:42 +0100

    Implement Validations in 'create.cshtml.cs'.

--- 09.MiniProject/BookRazorList/BookRazorList/Pages/TheBookList/Create.cshtml.cs
+++ 09.MiniProject/BookRazorList/BookRazorList/Pages/TheBookList/Create.cshtml.cs
@@ -18,10 +18,25 @@ namespace BookRazorList.Pages.TheBookList
             _db = db;  // 24.
         }
 
-		public TheBook TheBook { get; set; }  // 25. Add prop
+        [BindProperty]    // 38. binds the property of OnPost() without needding to passing in the values 
+        public TheBook TheBook { get; set; }  // 25. Add prop
 
 		public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()  // 31.
+        {
+            if (ModelState.IsValid)  // 32. if the model state is valid then ..
+            {
+                await _db.TheBook.AddAsync(TheBook);  // 33. add the book to the db
+                await _db.SaveChangesAsync();  // 34. pushed to the db
+                return RedirectToPage("Index");  // 35. redirect to the Index page
+            }
+            else  // 36.else
+            {
+                return Page(); // 37. e return to page
+            }
         }
     }
 }
