d4ee1c3ef9d4ff30234214859ff3e5afbd1e05a5
Joshhortt <joshhortt@yahoo.com>
2021-03-30 15:05:39 +0100

    Implement OnPostDelete Book handler.

--- 09.MiniProject/BookRazorList/BookRazorList/Pages/TheBookList/Index.cshtml.cs
+++ 09.MiniProject/BookRazorList/BookRazorList/Pages/TheBookList/Index.cshtml.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookRazorList.Pages.TheBookList
 {
-    public class IndexModel : PageModel  
+    public class IndexModel : PageModel
     {
         // include dependency injection
         private readonly ApplicationDbContext _db;  // 12.  add ApplicationDbContext to using BookRazorList.Model;
@@ -17,13 +17,26 @@ namespace BookRazorList.Pages.TheBookList
         public IndexModel(ApplicationDbContext db)  // 13. Constructor -->  add ApplicationDbContext to using BookRazorList.Model;
         {
             _db = db;  // 14.
-		}
+        }
 
-		public IEnumerable<TheBook> TheBooks { get; set; }   // 15. 
+        public IEnumerable<TheBook> TheBooks { get; set; }   // 15. 
 
-		public async Task OnGet()  // 17.  change void to async Task in the Get handler
+        public async Task OnGet()  // 17.  change void to async Task in the Get handler
         {
             TheBooks = await _db.TheBook.ToListAsync();  // 16. add await _db.TheBook.ToListAsync() --  using Microsoft.EntityFrameworkCore;
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)  // 61. 
+        {
+            var theBook = await _db.TheBook.FindAsync(id);  // 62.
+            if (theBook == null)  // 63.
+            {
+                return NotFound();  // 64.
+            }
+            _db.TheBook.Remove(theBook);  // 65.
+            await _db.SaveChangesAsync();  // 66.
+
+            return RedirectToPage("Index");  // 67.
         }
     }
 }
