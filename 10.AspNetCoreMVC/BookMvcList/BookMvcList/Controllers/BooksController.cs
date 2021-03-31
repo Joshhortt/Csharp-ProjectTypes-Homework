701161e84b5a4251e7745d41995e631aec0fca14
Joshhortt <joshhortt@yahoo.com>
2021-03-31 11:48:17 +0100

    Add Third party links to 'Layout.cshtml', add to'wwwroot' file 'bookList.js' and add API Calls in 'BooksController.cs'

--- 10.AspNetCoreMVC/BookMvcList/BookMvcList/Controllers/BooksController.cs
+++ 10.AspNetCoreMVC/BookMvcList/BookMvcList/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookMvcList.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,39 @@ namespace BookMvcList.Controllers
 {
 	public class BooksController : Controller
 	{
-		public IActionResult Index()  // 16. Right-click over Index and add a razor view called Index.
+        private readonly ApplicationDbContext _db;  // 19. 
+        [BindProperty]
+        public TheBook Book { get; set; }  // 20. Add property
+        public BooksController(ApplicationDbContext db)  // 21. Add Constructor
+        {
+            _db = db;  // 22. 
+        }
+
+        public IActionResult Index()  // 16. Right-click over Index and add a razor view called Index.
 		{
 			return View();
 		}
-	}
+
+        #region API Calls  
+        // 18. Copy paste Api Calls form the Razor Application in here
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });  // 23. ctrl + . MicrosoftEntityFrameworkCore
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Error when Deleting" });
+            }
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete was successful" });
+        }
+        #endregion
+    }
 }
