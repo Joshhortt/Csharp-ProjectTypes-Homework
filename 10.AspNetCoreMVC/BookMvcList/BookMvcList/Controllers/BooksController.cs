f0882df10cdc75b580fafc7bb94b558ef47e9348
Joshhortt <joshhortt@yahoo.com>
2021-03-31 11:06:08 +0100

    Create the 'BooksController' and add Index Razor View.

---
+++ 10.AspNetCoreMVC/BookMvcList/BookMvcList/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// 15. Create a new Controller + F5 to test it. Next we need to add a View - right-click Index and add view.
+namespace BookMvcList.Controllers
+{
+	public class BooksController : Controller
+	{
+		public IActionResult Index()  // 16. Right-click over Index and add a razor view called Index.
+		{
+			return View();
+		}
+	}
+}
