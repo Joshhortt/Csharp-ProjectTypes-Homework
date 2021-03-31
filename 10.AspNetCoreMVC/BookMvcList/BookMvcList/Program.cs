2d078b792e34043c0303532c9f8848c848b639a0
Joshhortt <joshhortt@yahoo.com>
2021-03-31 09:36:22 +0100

    First Commit of project 10. Asp Net Core MVC Homework 'BookMvcList'.

---
+++ 10.AspNetCoreMVC/BookMvcList/BookMvcList/Program.cs
@@ -0,0 +1,30 @@
+// HOMEWORK - 10 - ASP .NET Core MVC
+// Build a  ASP .NET Core MVC App Book List. 
+// Implement CRUD operations.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookMvcList
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
+}
