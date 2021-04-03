776dd614735310c8169ce49cfc1fcc5ffc4ab4ba
Joshhortt <joshhortt@yahoo.com>
2021-04-03 17:57:23 +0100

    Binding the Dependency Injection configuration.

--- 13.MiniProject/SupermarketAPI/SupermarketAPI/Startup.cs
+++ 13.MiniProject/SupermarketAPI/SupermarketAPI/Startup.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SupermarketAPI.Controllers;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using SupermarketAPI.Domain.Repositories;
+using SupermarketAPI.Domain.Services;
+using SupermarketAPI.Persistence.Contexts;
+using SupermarketAPI.Persistence.Repositories;
+using SupermarketAPI.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,46 +21,42 @@ using System.Threading.Tasks;
 
 namespace SupermarketAPI
 {
-	public class Startup
-	{
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
 
-		public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
+		[Obsolete]
 		public void ConfigureServices(IServiceCollection services)
-		{
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
 
-			services.AddControllers();
-			services.AddSwaggerGen(c =>
-			{
-				c.SwaggerDoc("v1", new OpenApiInfo { Title = "SupermarketAPI", Version = "v1" });
-			});
-		}
+            services.AddDbContext<AppDbContext>(options => {
+				options.UseInMemoryDatabase("supermarket-api-in-memory");
+            });
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SupermarketAPI v1"));
-			}
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
+        }
 
-			app.UseHttpsRedirection();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseRouting();
-
-			app.UseAuthorization();
-
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
-		}
-	}
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
 }
