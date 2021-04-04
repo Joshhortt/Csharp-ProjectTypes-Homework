211c895a28a41b7c7c014a05ee92b40835c5c527
Joshhortt <joshhortt@yahoo.com>
2021-04-04 19:16:51 +0100

    First Commit. Add Contact class with properties to Models.

---
+++ 11.MiniProject/WebApiDotNet5/WebApiDotNet5/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDotNet5.Models
+{
+	public class Contact
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string NichName { get; set; }
+		public DateTime DateCreated { get; set; }
+		public bool IsDeleted { get; set; }
+	}
+}
